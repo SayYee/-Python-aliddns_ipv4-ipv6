fcfb36d4636f22b53f38ad4af4276d483abd408f
SayYee <34704485+SayYee@users.noreply.github.com>
2023-04-09 21:03:36 +0800

    Update aliddns.py
    支持更新多个域名记录

--- aliddns.py
+++ aliddns.py
@@ -7,13 +7,13 @@ import requests
 from urllib.request import urlopen
 import json
 
-ipv4_flag = 1  # 是否开启ipv4 ddns解析,1为开启，0为关闭
+ipv4_flag = 0  # 是否开启ipv4 ddns解析,1为开启，0为关闭
 ipv6_flag = 1  # 是否开启ipv6 ddns解析,1为开启，0为关闭
 accessKeyId = "accessKeyId"  # 将accessKeyId改成自己的accessKeyId
 accessSecret = "accessSecret"  # 将accessSecret改成自己的accessSecret
 domain = "zeruns.tech"  # 你的主域名
-name_ipv4 = "blog"  # 要进行ipv4 ddns解析的子域名
-name_ipv6 = "ipv6.test"  # 要进行ipv6 ddns解析的子域名
+name_ipv4_list = ["blog"]  # 要进行ipv4 ddns解析的子域名
+name_ipv6_list = ["www", "notify"]  # 要进行ipv6 ddns解析的子域名
 
 
 client = AcsClient(accessKeyId, accessSecret, 'cn-hangzhou')
@@ -39,71 +39,74 @@ def add(DomainName, RR, Type, Value):  # 添加新的域名解析记录
     request.set_Value(Value)    
     response = client.do_action_with_exception(request)
 
-
-if ipv4_flag == 1:
+def processIpv4(ipv4, subDomain):
     request = DescribeSubDomainRecordsRequest()
     request.set_accept_format('json')
     request.set_DomainName(domain)
-    request.set_SubDomain(name_ipv4 + '.' + domain)
+    request.set_SubDomain(subDomain + '.' + domain)
     request.set_Type("A")
     response = client.do_action_with_exception(request)  # 获取域名解析记录列表
     domain_list = json.loads(response)  # 将返回的JSON数据转化为Python能识别的
 
-    ip = urlopen('https://api-ipv4.ip.sb/ip').read()  # 使用IP.SB的接口获取ipv4地址
-    ipv4 = str(ip, encoding='utf-8')
-    print("获取到IPv4地址：%s" % ipv4)
-
     if domain_list['TotalCount'] == 0:
-        add(domain, name_ipv4, "A", ipv4)
-        print("新建域名解析成功")
+        add(domain, subDomain, "A", ipv4)
+        print(subDomain + " 新建域名解析成功")
     elif domain_list['TotalCount'] == 1:
         if domain_list['DomainRecords']['Record'][0]['Value'].strip() != ipv4.strip():
-            update(domain_list['DomainRecords']['Record'][0]['RecordId'], name_ipv4, "A", ipv4)
-            print("修改域名解析成功")
+            update(domain_list['DomainRecords']['Record'][0]['RecordId'], subDomain, "A", ipv4)
+            print(subDomain + " 修改域名解析成功")
         else:  # https://blog.zeruns.tech
-            print("IPv4地址没变")
+            print(subDomain + " IPv4地址没变")
     elif domain_list['TotalCount'] > 1:
         from aliyunsdkalidns.request.v20150109.DeleteSubDomainRecordsRequest import DeleteSubDomainRecordsRequest
         request = DeleteSubDomainRecordsRequest()
         request.set_accept_format('json')
         request.set_DomainName(domain)  # https://blog.zeruns.tech
-        request.set_RR(name_ipv4)
+        request.set_RR(subDomain)
         request.set_Type("A") 
         response = client.do_action_with_exception(request)
-        add(domain, name_ipv4, "A", ipv4)
-        print("修改域名解析成功")
+        add(domain, subDomain, "A", ipv4)
+        print(subDomain + " 修改域名解析成功")
 
-print("本程序版权属于zeruns，博客：https://blog.zeruns.tech")
-
-if ipv6_flag == 1:
+def processIpv6(ipv6, subDomain):
     request = DescribeSubDomainRecordsRequest()
     request.set_accept_format('json')
     request.set_DomainName(domain)
-    request.set_SubDomain(name_ipv6 + '.' + domain)
+    request.set_SubDomain(subDomain + '.' + domain)
     request.set_Type("AAAA")
     response = client.do_action_with_exception(request)  # 获取域名解析记录列表
     domain_list = json.loads(response)  # 将返回的JSON数据转化为Python能识别的
 
-    ip = urlopen('https://api-ipv6.ip.sb/ip').read()  # 使用IP.SB的接口获取ipv6地址
-    ipv6 = str(ip, encoding='utf-8')
-    print("获取到IPv6地址：%s" % ipv6)
-
     if domain_list['TotalCount'] == 0:
-        add(domain, name_ipv6, "AAAA", ipv6)
-        print("新建域名解析成功")
+        add(domain, subDomain, "AAAA", ipv6)
+        print(subDomain + " 新建域名解析成功")
     elif domain_list['TotalCount'] == 1:
         if domain_list['DomainRecords']['Record'][0]['Value'].strip() != ipv6.strip():
-            update(domain_list['DomainRecords']['Record'][0]['RecordId'], name_ipv6, "AAAA", ipv6)
-            print("修改域名解析成功")
+            update(domain_list['DomainRecords']['Record'][0]['RecordId'], subDomain, "AAAA", ipv6)
+            print(subDomain + " 修改域名解析成功")
         else:  # https://blog.zeruns.tech
-            print("IPv6地址没变")
+            print(subDomain + " IPv6地址没变")
     elif domain_list['TotalCount'] > 1:
         from aliyunsdkalidns.request.v20150109.DeleteSubDomainRecordsRequest import DeleteSubDomainRecordsRequest
         request = DeleteSubDomainRecordsRequest()
         request.set_accept_format('json')
         request.set_DomainName(domain)
-        request.set_RR(name_ipv6)  # https://blog.zeruns.tech
+        request.set_RR(subDomain)  # https://blog.zeruns.tech
         request.set_Type("AAAA") 
         response = client.do_action_with_exception(request)
-        add(domain, name_ipv6, "AAAA", ipv6)
-        print("修改域名解析成功")
+        add(domain, subDomain, "AAAA", ipv6)
+        print(subDomain + " 修改域名解析成功")
+
+if ipv4_flag == 1:
+    ip = urlopen('https://api-ipv4.ip.sb/ip').read()  # 使用IP.SB的接口获取ipv4地址
+    ipv4 = str(ip, encoding='utf-8')
+    print("获取到IPv4地址：%s" % ipv4)
+    for subDomain in name_ipv4_list:
+        processIpv4(ipv4, subDomain)
+
+if ipv6_flag == 1:
+    ip = urlopen('https://api-ipv6.ip.sb/ip').read()  # 使用IP.SB的接口获取ipv6地址
+    ipv6 = str(ip, encoding='utf-8')
+    print("获取到IPv6地址：%s" % ipv6)
+    for subDomain in name_ipv6_list:
+        processIpv6(ipv6, subDomain)
